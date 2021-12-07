aa44e59dc9725408234d6ad7feb93991c1811082
jackup <yk.58@hotmail.com>
2021-12-07 17:10:48 +0300

    Add project files.

---
+++ MCD_GenericOncesiIslemler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList liste = new ArrayList();
+            //liste.Add();
+
+            VirtualDatabase vdb = new VirtualDatabase();
+            vdb.YeniKayit(1);
+            vdb.YeniKayit(2);
+
+        }
+    }
+}
---
+++ MCD_GenericOncesiIslemler/VirtualDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MCD_GenericOncesiIslemler
+{
+    public class VirtualDatabase
+    {
+        private ArrayList listem;
+
+        public VirtualDatabase()
+        {
+            listem = new ArrayList();
+        }
+        public void YeniKayit(int data)
+        {
+            listem.Add(data);
+        }
+    }
+}
